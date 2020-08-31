2eb1365e89057e1ffcbf2e00b259629eca8e4fec
Cleyton Chagas <cleyton-chagas@hotmail.com>
2020-08-31 16:43:00 -0300

    update patterns again

--- cadastro/src/main/java/com/cadastro/user/restservice/service/CadastrarServiceImpl.java
+++ cadastro/src/main/java/com/cadastro/user/restservice/service/CadastrarServiceImpl.java
@@ -1,5 +1,16 @@
 package com.cadastro.user.restservice.service;
 
-public class CadastrarServiceImpl {
+import org.springframework.stereotype.Component;
+
+import com.cadastro.user.restservice.dto.Cadastro;
+
+@Component
+public class CadastrarServiceImpl implements CadastrarService {
+
+	@Override
+	public Cadastro cadastrarUsuario(Cadastro usuario) {
+		System.out.println(usuario);
+		return usuario;
+	}
 
 }
